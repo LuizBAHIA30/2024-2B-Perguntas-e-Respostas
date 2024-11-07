e9810acfc30077623486309a6677345537cf9f68
Luiz Fernando <141454919+LuizBAHIA30@users.noreply.github.com>
2024-11-07 08:41:59 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    "Ciencia",
+    "Qual a massa do planeta Terra?",
+    "5,972 x 10^24 kg"
+)
+criaCartao(
+    "Biologia",
+    "Qual o maior orgao do ser humano?",
+    "A pele"
+)
+criaCartao(
+    "Historia",
+    "Qual foi a prmeira capital do Brasil?",
+    "Bahia"
+)
+criaCartao(
+    "Ciencia",
+    "Qual a massa do planeta Terra?",
+    "5,972 x 10^24 kg"
+)
+criaCartao(
+    "Biologia",
+    "Qual o maior orgao do ser humano?",
+    "A pele"
+)
+criaCartao(
+    "Historia",
+    "Qual foi a prmeira capital do Brasil?",
+    "Bahia")
